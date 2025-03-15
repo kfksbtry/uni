0276a843a017a11d3abd6af84eb5d6823a9291c5
kfksbtry <99765925+kfksbtry@users.noreply.github.com>
2025-03-15 17:42:15 +0300

    Create Hw1-Q4-a.c

---
+++ 1/2/ELE124/Hw1/Hw1-Q4-a.c
@@ -0,0 +1,33 @@
+/*Q4-Now solve the same problem using functions. Use the following prototypes:
+a)	int computeHomeValue (int popularity, int size)*/
+
+#include <stdio.h>
+#include <math.h>
+
+int computeHomeValue(int popularity, int size);
+
+int main(void) {
+	int popularity, size, homeValue; // Define variables without initializing
+	
+	printf("Enter the popularity: "); // Prompt 1
+	scanf("%d", &popularity); // Enter 1
+	
+	printf("Enter the size: "); // Prompt 2
+	scanf("%d", &size); // Enter 2
+		
+	printf("The home value is %d", computeHomeValue(popularity, size)); // Inform the user
+	
+	return 0; // Successfully terminate
+}
+
+int computeHomeValue(int popularity, int size) {
+	return (pow(popularity, 3) + pow(size, 2)) * 10000; // Calculate the value
+}
+
+/* PROGRAM STARTED
+
+Enter the popularity: 6
+Enter the size: 2
+The home value is 2200000
+
+PROGRAM ENDED */
